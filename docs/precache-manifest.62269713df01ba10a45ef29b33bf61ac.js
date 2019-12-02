dbfc6162420cad92cdd0358d0c1fb400447fa1a0
vklee88 <vklee88@gmail.com>
2019-12-02 14:52:12 -0800

    make production ready to serve

---
+++ docs/precache-manifest.62269713df01ba10a45ef29b33bf61ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fa611e4cd13ea89fcca275f51574a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b301d4c3c9a20818e1",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ad24de2856da77909587",
+    "url": "/static/js/2.f5f2b16d.chunk.js"
+  },
+  {
+    "revision": "c2b301d4c3c9a20818e1",
+    "url": "/static/js/main.5fbd4db7.chunk.js"
+  },
+  {
+    "revision": "ce790ed70f691b8e2d90",
+    "url": "/static/js/runtime-main.b8702738.js"
+  },
+  {
+    "revision": "91e98ccaf748dbeba35688c9fef2a7de",
+    "url": "/static/media/pvt_sleepiness.91e98cca.png"
+  },
+  {
+    "revision": "4154de8f0a0ebba7a5bf959904e09528",
+    "url": "/static/media/subjective_sleepiness.4154de8f.gif"
+  }
+]);
